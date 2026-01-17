5ccc5c4be0dc62c1882add05669f8976671f6774
Lesley Nthane <lesleynthane@ASUSPOWER.localdomain>
2026-01-17 23:34:55 +0200

    Create Spring boot backend app

---
+++ BusinessManagementSystem/src/test/java/lesDev/BusinessManagementSystem/BusinessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lesDev.BusinessManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
